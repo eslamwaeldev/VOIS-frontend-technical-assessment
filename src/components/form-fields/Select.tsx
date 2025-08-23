efa1728dbe3c940c7c30a4db17b2205405febd4c
Eslam Wael <eslam.wael104@gmail.com>
2025-08-24 00:32:01 +0300

    feat: build dynamic select input component

--- src/components/form-fields/Select.tsx
+++ src/components/form-fields/Select.tsx
@@ -1,5 +1,40 @@
-const Select = () => {
-  return <div>Select</div>;
+import { type HTMLAttributes } from "react";
+import ChevronDownIcon from "../../assets/icons/ChevronDownIcon";
+import type { Field } from "../../schema";
+import { useField } from "formik";
+
+export interface Props extends HTMLAttributes<HTMLSelectElement> {
+  field: Field;
+}
+
+const Select = ({ field, className, ...props }: Props) => {
+  const [fieldProps, meta] = useField(field.id);
+  return (
+    <div className="flex flex-col gap-1 relative">
+      <label htmlFor={field.id}>{field.label}</label>
+      <select
+        {...fieldProps}
+        name={field.id}
+        id={field.id}
+        value={fieldProps.value ? fieldProps.value : "disabled"}
+        className={`appearance-none h-11 rounded-lg border-2 p-2 cursor-pointer ${
+          meta.error ? "border-red-500" : "border-gray-700 dark:border-gray-300"
+        }  ${className}`}
+        {...props}
+      >
+        <option value="disabled">Select your answer</option>
+        {field.options?.map((option, index) => {
+          return (
+            <option key={`${index}- ${option}`} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+      <ChevronDownIcon className="absolute right-4 top-9.5 cursor-pointer pointer-events-none" />
+      {meta.error && <p className="text-red-500 text-sm">{meta.error}</p>}
+    </div>
+  );
 };
 
 export default Select;

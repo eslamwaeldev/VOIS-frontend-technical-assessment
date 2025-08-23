70922b5ad0269a996fef8d016b5191d73f887e09
Eslam Wael <eslam.wael104@gmail.com>
2025-08-23 20:08:23 +0300

    feat: add theme context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./router";
 import { HelmetProvider } from "react-helmet-async";
+import ThemeContextProvider from "./context/ThemeContextProvider";
 
 function App() {
   return (
     <HelmetProvider>
-      <RouterProvider router={router} />;
+      <ThemeContextProvider>
+        <RouterProvider router={router} />
+      </ThemeContextProvider>
     </HelmetProvider>
   );
 }

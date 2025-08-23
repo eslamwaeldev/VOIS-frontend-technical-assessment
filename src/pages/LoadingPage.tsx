78e22ef129d9d54803e4c92d99cd70747e8d760b
Eslam Wael <eslam.wael104@gmail.com>
2025-08-24 00:38:28 +0300

    feat: build fallback page

---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import Loading from "../components/Loading";
+
+const LoadingPage = () => {
+  return (
+    <div className="dark:bg-gray-950 dark:text-gray-300 min-h-screen flex items-center justify-center">
+      <Loading />
+    </div>
+  );
+};
+
+export default LoadingPage;

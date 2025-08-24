67e8c7d54d34e91a97e01d2b8af637a526b22061
Eslam Wael <eslam.wael104@gmail.com>
2025-08-25 01:14:49 +0300

    feat: add value consistency by saving to local storage

--- src/components/form-fields/TextField.tsx
+++ src/components/form-fields/TextField.tsx
@@ -1,12 +1,22 @@
 import { useField } from "formik";
 import type { Field } from "../../schema";
+import type { ChangeEvent } from "react";
+import useLocalStorageValues from "../../hooks/useLocalStorageValues";
 
 export interface Props {
   field: Field;
 }
 
 const TextField = ({ field }: Props) => {
-  const [fieldProps, meta] = useField(field.id);
+  const [fieldProps, meta, helpers] = useField(field.id);
+  const saveToLocalStorage = useLocalStorageValues(field.id, helpers.setValue);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    helpers.setValue(value);
+    saveToLocalStorage(value);
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={field.id}>{field.label}</label>
@@ -19,6 +29,7 @@ const TextField = ({ field }: Props) => {
         className={`h-11 rounded-lg border-2 p-2 cursor-pointer ${
           meta.error ? "border-red-500" : "border-gray-700 dark:border-gray-300"
         }`}
+        onChange={handleChange}
       />
       {meta.error && <p className="text-red-500 text-sm">{meta.error}</p>}
     </div>

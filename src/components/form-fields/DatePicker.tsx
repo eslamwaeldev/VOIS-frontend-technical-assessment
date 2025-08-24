dd4887d3d9b4e22951f25803bde32e67145802f3
Eslam Wael <eslam.wael104@gmail.com>
2025-08-24 22:52:42 +0300

    feat: build date picker component

---
+++ src/components/form-fields/DatePicker.tsx
@@ -0,0 +1,44 @@
+import { useField } from "formik";
+import { useRef, type ChangeEvent } from "react";
+import Calendar from "../../assets/icons/Calendar";
+import type { Field } from "../../schema";
+
+export interface Props {
+  field: Field;
+}
+
+const DatePicker = ({ field }: Props) => {
+  const [fieldProps, meta, helpers] = useField(field.id);
+  const dateInputRef = useRef<HTMLInputElement | null>(null);
+  const handleClick = () => {
+    if (dateInputRef.current) {
+      dateInputRef.current.showPicker();
+    }
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    helpers.setValue(value);
+  };
+  return (
+    <div className={`flex flex-col gap-2 relative cursor-pointer`}>
+      <label htmlFor={field.id}>{field.label}</label>
+      <input
+        {...fieldProps}
+        type="date"
+        ref={dateInputRef}
+        name={field.id}
+        id={field.id}
+        value={fieldProps.value ? fieldProps.value : ""}
+        className={`h-11 appearance-none rounded-lg border-2 p-2 cursor-pointer ${
+          meta.error && fieldProps.value ? "border-red-500" : "border-gray-700 dark:border-gray-300"
+        }`}
+        onClick={handleClick}
+        onChange={handleChange}
+      />
+      <Calendar className="absolute right-4 top-9.5 !cursor-pointer pointer-events-none" />
+      {meta.error && fieldProps.value && <p className="text-red-500 text-sm">{meta.error}</p>}
+    </div>
+  );
+};
+
+export default DatePicker;

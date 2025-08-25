2737274612940c951ef21b08ff43433ebcf23e5a
Eslam Wael <eslam.wael104@gmail.com>
2025-08-25 20:07:21 +0300

    feat: build textarea input

---
+++ src/components/form-fields/TextArea.tsx
@@ -0,0 +1,39 @@
+import { useField } from "formik";
+import type { Field } from "../../schema";
+import type { ChangeEvent } from "react";
+import useLocalStorageValues from "../../hooks/useLocalStorageValues";
+
+export interface Props {
+  field: Field;
+}
+
+const TextArea = ({ field }: Props) => {
+  const [fieldProps, meta, helpers] = useField(field.id);
+  const saveToLocalStorage = useLocalStorageValues(field.id, helpers.setValue);
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.currentTarget;
+    helpers.setValue(value);
+    saveToLocalStorage(value);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={field.id}>{field.label}</label>
+      <textarea
+        {...fieldProps}
+        name={field.id}
+        id={field.id}
+        cols={30}
+        rows={10}
+        onChange={handleChange}
+        className={`h-11 rounded-lg border-2 p-2 cursor-pointer ${
+          meta.error ? "border-vodafone-red" : "border-gray-700 dark:border-gray-300"
+        }`}
+      ></textarea>
+      {meta.error && <p className="text-red-500 text-sm">{meta.error}</p>}
+    </div>
+  );
+};
+
+export default TextArea;

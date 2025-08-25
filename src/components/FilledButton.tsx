832fa9bda918d52fa8dc22c96f8d03d033deaa94
Eslam Wael <eslam.wael104@gmail.com>
2025-08-26 01:46:30 +0300

    feat: add text color

--- src/components/FilledButton.tsx
+++ src/components/FilledButton.tsx
@@ -9,7 +9,7 @@ const FilledButton = ({
 >) => {
   return (
     <button
-      className={` h-11 shadow-sm  border-gray-300 bg-vodafone-gray dark:bg-gray-100 dark:text-vodafone-red hover:bg-red-500 hover:text-gray-50 rounded-lg p-2 transition-all duration-150 font-medium ${className}`}
+      className={` h-11 shadow-sm text-gray-50 border-gray-300 bg-vodafone-gray dark:bg-gray-100 dark:text-vodafone-red hover:bg-red-500 hover:text-gray-50 rounded-lg p-2 transition-all duration-150 font-medium ${className}`}
       {...props}
     >
       {children}
